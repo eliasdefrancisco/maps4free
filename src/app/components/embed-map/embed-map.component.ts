b2ec93d97f9363d1db559645e7e0c7b6c7ed30a4
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-10 13:18:50 +0200

    Auto updating ubication on embeded map

--- src/app/components/embed-map/embed-map.component.ts
+++ src/app/components/embed-map/embed-map.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component,
+  OnInit
+} from '@angular/core'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-embed-map',
@@ -6,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./embed-map.component.scss'],
 })
 export class EmbedMapComponent implements OnInit {
+  gMapSrcSanitized: SafeResourceUrl
 
-  constructor() { }
+  private set gMapSrc(url: string) {
+    this.gMapSrcSanitized = this.sanitizer.bypassSecurityTrustResourceUrl(url)
+  }
 
-  ngOnInit() {}
+  constructor(private sanitizer: DomSanitizer) { }
+  
+  ngOnInit() {
+    let lat = -6.09969875
+    let lng = 36.6798263
+    this.setUrlUbi(lat, lng)
+    setInterval(() => {
+      lat -= 0.002
+      lng += 0.002
+      this.setUrlUbi(lat, lng)
+    },20000)
+  }
+    
+  private setUrlUbi(lat: number, lng: number) {
+    this.gMapSrc = `\
+      https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12799.114010794727\
+      !2d${lat.toString()}\
+      !3d${lng.toString()}\
+      !2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1565373576240!5m2!1ses!2ses\
+    `
+  }
 
 }
