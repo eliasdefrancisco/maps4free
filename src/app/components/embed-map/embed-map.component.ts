59b32bd4b36d46db4374cf81034114f187b440e5
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-12 21:37:37 +0200

    Smooth following on embed map

--- src/app/components/embed-map/embed-map.component.ts
+++ src/app/components/embed-map/embed-map.component.ts
@@ -13,11 +13,36 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 export class EmbedMapComponent implements OnInit {
   @Input() hideControls: boolean = true
   @Input() showLocator: boolean = true
-  @Input() updatePositionSeconds: number = 30
-  gMapSrcSanitized: SafeResourceUrl
+  @Input() updatePositionSeconds: number = 10
+  gMapSrcSanitized_0: SafeResourceUrl
+  gMapSrcSanitized_1: SafeResourceUrl
+  iframeSwitcher: boolean = true
+  iframeSwitcherShow: boolean = true
+  iframeIntervalTime = 3000
+  isFirstTime = true
+  visLocator = false
 
   private set gMapSrc(url: string) {
-    this.gMapSrcSanitized = this.sanitizer.bypassSecurityTrustResourceUrl(url)
+    const gMapSrcSanitized = this.sanitizer.bypassSecurityTrustResourceUrl(url)
+    if (this.isFirstTime) {
+      this.gMapSrcSanitized_1 = gMapSrcSanitized
+      this.gMapSrcSanitized_0 = gMapSrcSanitized
+      this.isFirstTime = false
+    }
+    else {
+      if (this.iframeSwitcher) {
+        this.gMapSrcSanitized_1 = gMapSrcSanitized
+      }
+      else {
+        this.gMapSrcSanitized_0 = gMapSrcSanitized
+      }
+      setTimeout(() => {
+        this.iframeSwitcher = !this.iframeSwitcher
+        setTimeout(() => {
+          this.iframeSwitcherShow = !this.iframeSwitcherShow
+        }, this.iframeIntervalTime)
+      }, this.iframeIntervalTime)
+    }
   }
 
   constructor(
@@ -25,19 +50,26 @@ export class EmbedMapComponent implements OnInit {
   ) { }
   
   ngOnInit() {
-    this.setMap()
+    this.initMap()
+    this.initLocator()
+  }
+
+  private initLocator() {
+    if(this.showLocator){
+      setTimeout(() => this.visLocator = true, this.iframeIntervalTime)
+    }
   }
   
-  private setMap() {
+  private initMap() {
     let lat = -6.09969875
     let lng = 36.6798263
     this.setUrlUbi(lat, lng)
     if(this.updatePositionSeconds > 0) {
       setInterval(() => {
-        lat -= 0.002
-        lng += 0.002
+        lat -= 0.0005
+        lng += 0.0005
         this.setUrlUbi(lat, lng)
-      }, this.updatePositionSeconds * 1000)
+      }, this.updatePositionSeconds * 1000 + this.iframeIntervalTime * 2)
     }
   }
     
